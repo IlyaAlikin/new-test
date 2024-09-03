c6dc629526c84dede90483ae4bc05cea7b7de55b
Ilya Alikin <iljuha.alikin@gmail.com>
2024-09-03 17:35:17 +0500

    add first unit-test

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel'
-}
+  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  transform: {
+    "^.+\\.tsx?$": "babel-jest", // or ts-jest for TypeScript
+    "^.+\\.jsx?$": "babel-jest",
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!axios)/", // Transpile axios and other ESM modules
+  ],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
