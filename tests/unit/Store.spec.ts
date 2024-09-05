cd7960a5522904089e07501287981ac72179ab0d
Ilya Alikin <iljuha.alikin@gmail.com>
2024-09-05 13:28:42 +0500

    add setCharacter testing

--- tests/unit/Store.spec.ts
+++ tests/unit/Store.spec.ts
@@ -22,7 +22,33 @@ describe("Characters Store", () => {
 
     await characterStore.loadStarships();
 
-    expect(characterStore.starships).toHaveLength(2);
+    expect(characterStore.starships).toHaveLength(mockStarships.length);
     expect(characterStore.starships).toEqual(mockStarships);
+  });
+
+  it("set characters and add it to each character in the store", () => {
+    const mockCharacters = [
+      { name: "Luke Skywalker", birth_year: "19BBY", starships: [] },
+      { name: "Leia Organa", birth_year: "19BBY", starships: [] },
+      { name: "Darth Vader", birth_year: "42BBY", starships: [] },
+    ];
+
+    (axios.get as jest.Mock).mockResolvedValue({
+      data: { results: mockCharacters },
+    });
+
+    const characterStore = useCharactersStore();
+
+    expect(characterStore.characters).toHaveLength(0);
+    characterStore.setCharacters(mockCharacters);
+
+    let counter = 1;
+
+    characterStore.characters.forEach((element) => {
+      expect(element.id).toBe(counter);
+      counter++;
+    });
+
+    expect(characterStore.characters).toHaveLength(mockCharacters.length);
   });
 });
